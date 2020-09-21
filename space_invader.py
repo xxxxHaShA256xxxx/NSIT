6a71e14a415594202aa99b3fe065985efe4a6ca5
xxxxHaShA256xxxx <64052909+xxxxHaShA256xxxx@users.noreply.github.com>
2020-09-21 19:54:05 +0200

    Update space_invader.py

--- space_invader.py
+++ space_invader.py
@@ -1,1 +1,130 @@
+import pygame
+import random
+import space
+import sys
+
+class joueur:
+    def __init__(self):
+        self.position = 400
+        self.image = pygame.image.load("vaisseau.png")
+        self.sens = "O"
+        self.vitesse = 10
+        self.score = 0
+    def deplacer(self):
+        if (self.sens == 'droite') and (self.positions < 740):
+            self.positions += vitesse
+        elif (self.sens == 'gauche') and(self.positions > 0):
+            self.positions -= vitesse
+    def tirer(self):
+        self.sens = "O"
+    def marquer(self):
+        self.score = self.score + 1
+class Balle:
+    def __init__(self):
+        self.tireur = tireur
+        self.depart = tireur.positions + 16
+        self.hauteur = 492
+        self.image = pygame.image.load("balle.png")
+        self.etat = "charger"
+        self.vitesse = 5
+    def bouger(self):
+        if self.etat == "attente":
+            self.depart = self.tireur.positions + 16
+            self.hauteur = 492
+        elif self.etat == "tiree":
+            self.hauteur -= 5
+        if self.hauteur < 0:
+            self.etat = "charger"
+    def toucher(self):
+        if (math.fabs(self.hauteur - vaisseau.hauteur) < 40) and (math.fabs(self.depart - vaisseau.depart) < 40):
+            self.etat = "chargee"
+            return True
+class Ennemi:
+    Nombre_Ennemie = 10
+    def __init__(self):
+        self.depart = random.randint(1,700)
+        self.hauteur = 10
+        self.type = random.randint(1,2)
+        if(self.type == 1):
+            self.image = pygame.image.load("invader1.png")
+            self.vitesse = 1
+        elif(self.type == 2):
+            self.image = pygame.image.load("invader2.png")
+            self.vitesse = 2
+            
+    def avancer(self):
+        self.hauteur += self.vitesse
+    def disparaitre(self):
+        self.depart = random.randint(1,700)
+        self.hauteur = 10
+        self.type = random.randint(1,2)
+        if  (self.type == 1):
+            self.image = pygame.image.load("invader1.png")
+            self.vitesse = 1
+        elif (self.type ==2):
+            self.image = pygame.image.load("invader2.png")
+            self.vitesse = 2
+
+pygame.init() 
+
+
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Space Invaders") 
+
+fond = pygame.image.load('background.png')
+
+
+player = space.Joueur()
+
+tir = space.Balle(player)
+tir.etat = "chargee"
+
+listeEnnemis = []
+for indice in range(space.Ennemi.NbEnnemis):
+    vaisseau = space.Ennemi()
+    listeEnnemis.append(vaisseau)
+    
+
+running = True 
+
+while running : 
+    
+    screen.blit(fond,(0,0))
+
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT : 
+            running = False 
+            sys.exit() 
+       
+       
+        if event.type == pygame.KEYDOWN : 
+            if event.key == pygame.K_LEFT:
+                player.sens = "gauche" 
+            if event.key == pygame.K_RIGHT :  
+                player.sens = "droite" 
+            if event.key == pygame.K_SPACE : 
+                player.tirer()
+                tir.etat = "tiree"
+
+   
+    for ennemi in listeEnnemis:
+        if tir.toucher(ennemi):
+            ennemi.disparaitre()
+            player.marquer()
+    print(f"Score = {player.score} points")
+    
+    
+    player.deplacer()
+    screen.blit(tir.image,[tir.depart,tir.hauteur])        
+    
+    tir.bouger()
+    screen.blit(player.image,[player.position,500]) 
+    
+    for ennemi in listeEnnemis:
+        ennemi.avancer()
+        screen.blit(ennemi.image,[ennemi.depart, ennemi.hauteur]) 
+        
+    pygame.display.update() 
+
 
